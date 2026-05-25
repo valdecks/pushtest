b116900f8afc7b7202983763cd89c72c2a25a568
valdecks <valdecksan@gmail.com>
2026-05-25 19:48:00 -0300

    sexto  Commit

--- aula4.py
+++ aula4.py
@@ -1,9 +1,2 @@
-print("Aula4")
-<<<<<<< HEAD
-print("Aula4 git")
-=======
-print("Aula4 git")
-print("teste1")
-print("teste2")
-print("teste3")
->>>>>>> master
+Print("Novo arquivo")
+Print("Ultima Alteracao")
